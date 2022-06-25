34f4c1b281d92bef14e7af0c7d31cfdd6686a7c1
Salva Pérez <sperher@mercadona.es>
2022-06-25 13:55:19 +0200

    Base template code

---
+++ src/main/java/com/salpreh/dbexp/HibernateExpApplication.java
@@ -0,0 +1,13 @@
+package com.salpreh.dbexp;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class HibernateExpApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(HibernateExpApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/salpreh/dbexp/HibernateExpApplicationTests.java
@@ -0,0 +1,13 @@
+package com.salpreh.dbexp;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class HibernateExpApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
