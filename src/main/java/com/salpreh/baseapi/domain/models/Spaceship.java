7b90d0dd3f17f05cf79b19ef85d0ddccd300022c
Salva Pérez <sperher@mercadona.es>
2022-07-16 18:25:21 +0200

    Added domain models

---
+++ src/main/java/com/salpreh/baseapi/domain/models/Spaceship.java
@@ -0,0 +1,28 @@
+package com.salpreh.baseapi.domain.models;
+
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+import java.util.HashSet;
+import java.util.Set;
+
+@Data
+@Builder
+@AllArgsConstructor
+@NoArgsConstructor
+public class Spaceship {
+    private Long id;
+    private String name;
+    private Planet assignedPort;
+    private Faction affiliation;
+    private Set<Assignation> crew = new HashSet<>();
+
+    public void setAffiliation(Faction faction) {
+        if (this.affiliation != null) this.affiliation.removeSpaceship(this);
+        if (faction != null) faction.addSpaceship(this);
+
+        this.affiliation = faction;
+    }
+}
