904adbc153bacd77ba69e4b544209ccf46c99ce1
salpreh <salpreh.7@gmail.com>
2022-08-11 14:24:30 +0200

    Exposed revision metadata in use case and controller

--- src/main/java/com/salpreh/baseapi/adapters/application/api/controllers/PersonAuditController.java
+++ src/main/java/com/salpreh/baseapi/adapters/application/api/controllers/PersonAuditController.java
@@ -4,7 +4,8 @@ import com.salpreh.baseapi.adapters.application.api.config.PaginationConfig;
 import com.salpreh.baseapi.adapters.application.api.mappers.ApiMapper;
 import com.salpreh.baseapi.adapters.application.api.models.ApiPage;
 import com.salpreh.baseapi.domain.models.Person;
-import com.salpreh.baseapi.domain.models.commons.RevisionData;
+import com.salpreh.baseapi.domain.models.revisions.RevisionData;
+import com.salpreh.baseapi.domain.models.revisions.RevisionModel;
 import com.salpreh.baseapi.domain.ports.application.PersonRevisionUseCase;
 import lombok.RequiredArgsConstructor;
 import org.springframework.data.domain.PageRequest;
@@ -21,7 +22,7 @@ public class PersonAuditController {
   private final ApiMapper mapper;
 
   @GetMapping("{id}")
-  public ApiPage<Person> getAllRevisions(
+  public ApiPage<RevisionModel<Person>> getAllRevisions(
     @PathVariable("id") long personId,
     @RequestParam(defaultValue = PaginationConfig.DEFAULT_PAGE) int page,
     @RequestParam(defaultValue = PaginationConfig.DEFAULT_PAGE_SIZE) int pageSize
@@ -43,7 +44,7 @@ public class PersonAuditController {
   }
 
   @GetMapping("{id}/{revisionId}")
-  public Person getRevision(@PathVariable("id") long personId, @PathVariable long revisionId) {
+  public RevisionModel<Person> getRevision(@PathVariable("id") long personId, @PathVariable long revisionId) {
     return personRevisionUseCase.findRevision(personId, revisionId)
       .orElseThrow(() -> new ResponseStatusException(HttpStatus.NOT_FOUND));
   }
