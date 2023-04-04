da05dc8bdd04e1323bf7e888c0fd4aef61ba54a7
salpreh <salva.perez46@gmail.com>
2023-04-04 14:31:50 +0200

    Added request params validations

--- src/main/java/com/salpreh/baseapi/adapters/application/api/controllers/PersonController.java
+++ src/main/java/com/salpreh/baseapi/adapters/application/api/controllers/PersonController.java
@@ -6,15 +6,26 @@ import com.salpreh.baseapi.adapters.application.api.models.ApiPage;
 import com.salpreh.baseapi.domain.models.Person;
 import com.salpreh.baseapi.domain.models.commands.PersonCreateCommand;
 import com.salpreh.baseapi.domain.ports.application.PersonUseCase;
+import javax.validation.Valid;
+import javax.validation.constraints.Min;
 import lombok.RequiredArgsConstructor;
 import org.springframework.data.domain.PageRequest;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.*;
+import org.springframework.web.bind.annotation.DeleteMapping;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.PutMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.RestController;
 import org.springframework.web.server.ResponseStatusException;
 
 @RestController
 @RequiredArgsConstructor
+@Valid
 @RequestMapping("/person")
 public class PersonController {
 
@@ -23,8 +34,8 @@ public class PersonController {
 
   @GetMapping
   public ApiPage<Person> getAll(
-    @RequestParam(defaultValue = PaginationConfig.DEFAULT_PAGE) int page,
-    @RequestParam(defaultValue = PaginationConfig.DEFAULT_PAGE_SIZE) int pageSize
+    @Min(0) @RequestParam(defaultValue = PaginationConfig.DEFAULT_PAGE) int page,
+    @Min(1) @RequestParam(defaultValue = PaginationConfig.DEFAULT_PAGE_SIZE) int pageSize
   ) {
     var data = personUseCase.findAll(PageRequest.of(page, pageSize));
 
@@ -32,7 +43,7 @@ public class PersonController {
   }
 
   @GetMapping("{id}")
-  public Person get(@PathVariable long id) {
+  public Person get(@Min(1) @PathVariable long id) {
     return personUseCase.findById(id)
       .orElseThrow(() -> new ResponseStatusException(HttpStatus.NOT_FOUND));
   }
@@ -43,12 +54,12 @@ public class PersonController {
   }
 
   @PutMapping("{id}")
-  public Person update(@PathVariable long id, @RequestBody PersonCreateCommand command) {
+  public Person update(@Min(1) @PathVariable long id, @RequestBody PersonCreateCommand command) {
     return personUseCase.updatePerson(id, command);
   }
 
   @DeleteMapping("{id}")
-  public ResponseEntity<Void> delete(@PathVariable long id) {
+  public ResponseEntity<Void> delete(@Min(1) @PathVariable long id) {
     personUseCase.deletePerson(id);
 
     return ResponseEntity.noContent()
