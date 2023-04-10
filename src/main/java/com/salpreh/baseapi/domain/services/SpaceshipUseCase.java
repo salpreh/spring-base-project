22a84c672ca77f5e844fc7a10852b2c35bf90199
salpreh <salva.perez46@gmail.com>
2023-04-10 18:29:13 +0200

    Context logging code in spaceship use case

--- src/main/java/com/salpreh/baseapi/domain/services/SpaceshipUseCase.java
+++ src/main/java/com/salpreh/baseapi/domain/services/SpaceshipUseCase.java
@@ -2,23 +2,30 @@ package com.salpreh.baseapi.domain.services;
 
 import com.salpreh.baseapi.domain.models.Spaceship;
 import com.salpreh.baseapi.domain.models.commands.SpaceshipCreateCommand;
+import com.salpreh.baseapi.domain.models.contexts.DeviceContextHolder;
+import com.salpreh.baseapi.domain.models.contexts.ProviderContext;
 import com.salpreh.baseapi.domain.ports.application.SpaceshipPort;
 import com.salpreh.baseapi.domain.ports.infrastructure.SpaceshipDatasourcePort;
 import java.util.Optional;
 import lombok.RequiredArgsConstructor;
+import lombok.extern.log4j.Log4j2;
 import org.springframework.data.domain.Page;
 import org.springframework.data.domain.Pageable;
 import org.springframework.stereotype.Service;
 
+@Log4j2
 @Service
 @RequiredArgsConstructor
 public class SpaceshipUseCase implements SpaceshipPort {
 
   private final SpaceshipDatasourcePort spaceshipDatasourcePort;
-
+  private final ProviderContext providerContext;
 
   @Override
   public Optional<Spaceship> findById(long id) {
+    log.info("Current DeviceId: {}", DeviceContextHolder.getDeviceId());
+    log.info("Current ProviderId: {}", providerContext.getProviderId());
+
     return spaceshipDatasourcePort.findById(id);
   }
 
